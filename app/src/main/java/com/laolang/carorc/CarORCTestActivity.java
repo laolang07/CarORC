c8771014f6e2ef8e3700da98e2dce1c6e690f41f
laolang <479926241@qq.com>
2019-01-28 16:52:32 +0800

    Update CarORCTestActivity.java

--- app/src/main/java/com/laolang/carorc/CarORCTestActivity.java
+++ app/src/main/java/com/laolang/carorc/CarORCTestActivity.java
@@ -10,4 +10,34 @@ public class CarORCTestActivity extends CarORCCaptureActivity {
         initCarORCAPI();//初始化API
         super.onCreate(savedInstanceState);
     }
+
+    /**
+     * 如果进行重新布局注意SurfaceViewID和com.laolang.libcarorc.ViewfinderViewID必须和原布局保持一致，其余根据实际功能需求使用
+     * @return
+     */
+    @Override
+    public int getLayoutId() {
+        return super.getLayoutId();
+    }
+
+    /**
+     * 开始采集图片进行解析
+     * @param msg
+     */
+    @Override
+    protected void StartScaning(String msg) {
+        super.StartScaning(msg);
+    }
+
+    /**
+     * 解析结果
+     * @param iSsuccess
+     * @param msg
+     * @param result
+     */
+    @Override
+    protected void Scanfinished(boolean iSsuccess, String msg, String result) {
+        super.Scanfinished(iSsuccess, msg, result);
+
+    }
 }
